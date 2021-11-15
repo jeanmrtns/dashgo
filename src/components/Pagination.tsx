015e5bebbb0c5e3829601091d6b82127896ccc9f
Jean Martins <jeanmrtns4@gmail.com>
2021-11-15 13:40:05 -0300

    Controle de usuarios

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,70 @@
+import { HStack, Button, Box } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <HStack mt="8" justify="space-between" align="center" spacing="6">
+        <Box>
+            <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+        </Box>
+      <HStack spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{ bgColor: "pink.500", cursor: "default" }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.600"
+          _hover={{
+            bgColor: "gray.500",
+          }}
+        >
+          2
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.600"
+          _hover={{
+            bgColor: "gray.500",
+          }}
+        >
+          3
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.600"
+          _hover={{
+            bgColor: "gray.500",
+          }}
+        >
+          4
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.600"
+          _hover={{
+            bgColor: "gray.500",
+          }}
+        >
+          5
+        </Button>
+      </HStack>
+    </HStack>
+  );
+}
---
+++ src/pages/users/create.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Divider,
+  VStack,
+  SimpleGrid,
+  HStack,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
+import { RiSaveLine, RiArrowLeftLine } from "react-icons/ri";
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input label="Nome completo" name="name" />
+              <Input label="E-mail" name="email" type="email" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input label="Senha" name="password" type="password" />
+              <Input
+                label="Confirmação da senha"
+                name="password_confirmation"
+                type="password"
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button
+                colorScheme="whiteAlpha"
+                leftIcon={<Icon as={RiArrowLeftLine} fontSize="20" />}
+              >
+                Cancelar
+              </Button>
+              <Button
+                colorScheme="pink"
+                leftIcon={<Icon as={RiSaveLine} fontSize="20" />}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
+export default CreateUser;
